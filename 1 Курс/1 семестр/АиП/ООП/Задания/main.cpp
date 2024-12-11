568341f673fafb25a59d73e9277897cb99facb72
Лиходиевский <jdh994lr@gmail.com>
2024-12-11 13:48:39 +0300

    Бонусы

--- 1 Курс/1 семестр/АиП/ООП/Задания/main.cpp
+++ 1 Курс/1 семестр/АиП/ООП/Задания/main.cpp
@@ -1,167 +1,223 @@
 #include <iostream>
-#include <cstring>
-#include <limits>
-#include <string>
 
-class Pet {
+class Pet
+{
 public:
+    // Два чисто виртуальных метода
     virtual std::string name() = 0;
     virtual std::string voice() = 0;
-    virtual double weight() = 0;
-    virtual std::string character() { return ""; }
-    virtual ~Pet() = default;
+    virtual std::string get_type() {return "Pet";}
+    virtual std::size_t weight() {return 0;}
 
-protected:
-    std::string name_, voice_;
-    double weight_;
+    friend std::ostream &operator<<(std::ostream &stream, Pet &pet)
+    {
+        std::cout << pet.get_type() << " " << pet.name() << " " << pet.voice() << " " << pet.weight() << std::endl;
+        return stream;
+    }
+    virtual ~Pet(){}
 };
 
-class Cat : public Pet {
+class Cat : public Pet
+{
 public:
-    Cat() = default;
-    Cat(std::string name, std::string voice, double weight) {
-        this->name_ = name;
-        this->voice_ = voice;
-        this->weight_ = weight;
-    }
-    std::string name() override { return name_; }
-    std::string voice() override { return voice_; }
-    double weight() override { return weight_; }
-};
+    // Конструктор
+    Cat(std::string name, std::string voice, int weight) : name_(name),
+                                                           voice_(voice),
+                                                           weight_(weight) {}
 
-class Dog : public Pet {
-public:
-    Dog(std::string name, std::string voice, double weight, std::string character = " ") {
-        this->name_ = name;
-        this->voice_ = voice;
-        this->weight_ = weight;
-        this->character_ = character;
+    // Имя
+    std::string name() override
+    {
+        return name_;
     }
-    std::string name() override { return name_; }
-    std::string voice() override { return voice_; }
-    double weight() override { return weight_; }
-    std::string character() override { return character_; }
+    // Голос
+    std::string voice() override
+    {
+        return voice_;
+    }
+    // Вес
+    std::size_t weight() override
+    {
+        return weight_;
+    }
+    // Тип (если нужен)
+    std::string get_type() override
+    {
+        return type;
+    }
 
 private:
+    std::string name_, voice_;
+    std::string type = "Cat";
+    std::size_t weight_;
+};
+
+// Скопировали кошку и назвали собакой, хоть по логике они одинаковы
+class Dog : public Pet
+{
+public:
+    // Конструктор
+    Dog(std::string name, std::string voice, int weight, std::string character = "pass") : name_(name),
+                                                                                           voice_(voice),
+                                                                                           weight_(weight),
+                                                                                           character_(character) {}
+
+    // Имя
+    std::string name() override
+    {
+        return name_;
+    }
+    // Голос
+    std::string voice() override
+    {
+        return voice_;
+    }
+    // Вес
+    std::size_t weight() override
+    {
+        return weight_;
+    }
+    // Тип (если нужен)
+    std::string get_type() override
+    {
+        return type;
+    }
+
+private:
+    std::string name_, voice_;
+    std::string type = "Dog";
+    std::size_t weight_;
     std::string character_;
 };
 
-class FamilyPets {
+// Задание
+class FamilyPets
+{
 public:
-    FamilyPets() = default;
-    void voice() {
-        for (int i = 0; i < current_length; i++) {
-            std::cout << family[i]->voice() << std::endl;
+    FamilyPets() {}
+
+    void operator+(Pet *pet)
+    {
+        if (curretnt_lenght < max_capacity)
+        {
+            family[curretnt_lenght] = pet;
+            curretnt_lenght++;
+        }
+        else
+        {
+            std::cerr << "ERROR: Maximum capacity reached" << std::endl;
         }
     }
-    void names() {
-        for (int i = 0; i < current_length; i++) {
+
+    void voice()
+    {
+        for (std::size_t i = 0; i < curretnt_lenght; i++)
+        {
+            std::cout << family[i]->name() << ": " << family[i]->voice() << std::endl;
+        }
+    }
+
+    void name()
+    {
+        for (std::size_t i = 0; i < curretnt_lenght; i++)
+        {
             std::cout << family[i]->name() << std::endl;
         }
     }
-    int getNumberRepetitionsName(std::string str) {
-        int counter = 0;
-        for (int i = 0; i < current_length; i++) {
-            if (family[i]->name() == str) {
-                counter++;
+
+    std::size_t getNumberRepetitionsName(std::string name)
+    {
+        std::size_t count = 0;
+        for (std::size_t i = 0; i < curretnt_lenght; i++)
+        {
+            if (name == family[i]->name())
+            {
+                count++;
             }
         }
-        return counter;
+        return count;
     }
-    FamilyPets& operator+=(Pet* pet) {
-        if (current_length < max_capacity) {
-            family[current_length++] = pet;
-        }
-        return *this;
-    }
-    std::string findTheEasiest() {
-        if (current_length == 0) return "";
-        double min_weight = std::numeric_limits<double>::max();
+    std::string findTheEasiest()
+    {
         std::size_t ind = 0;
-        for (int i = 0; i < current_length; i++) {
-            if (family[i]->weight() < min_weight) {
+        std::size_t easy_weight = family[ind]->weight();
+        for (std::size_t i = 0; i < curretnt_lenght; i++)
+        {
+            if (easy_weight > family[i]->weight())
+            {
                 ind = i;
-                min_weight = family[i]->weight();
             }
         }
         return family[ind]->name();
     }
-    friend std::ostream& operator<<(std::ostream& stream, FamilyPets& famPet) {
-        for (int i = 0; i < famPet.current_length; i++) {
-            stream << "Name: " << famPet.family[i]->name() << ", Voice: " << famPet.family[i]->voice() << ", Weight: " << famPet.family[i]->weight();
-            if (dynamic_cast<Dog*>(famPet.family[i])) {
-                stream << ", Character: " << famPet.family[i]->character();
-            }
-            stream << std::endl;
+
+    friend std::ostream &operator<<(std::ostream &stream, FamilyPets &fml)
+    {
+        for (std::size_t i = 0; i < fml.curretnt_lenght; i++)
+        {
+            stream << *fml.family[i];
         }
         return stream;
     }
-    ~FamilyPets() {
-        for (int i = 0; i < current_length; i++) {
-            delete family[i];
+
+    // Чистим память
+    ~FamilyPets()
+    {
+        for (std::size_t i = 0; i < curretnt_lenght; i++)
+        {
+            delete family[i]; // Освобождаем память
         }
     }
 
 private:
     std::size_t max_capacity{10};
-    std::size_t current_length{0};
-    Pet* family[10];
+    std::size_t curretnt_lenght{0};
+    Pet *family[10];
 };
 
-int main() {
-    int maxNPets;
-    std::cout << "Enter the maximum number of pets: ";
-    std::cin >> maxNPets;
-    if (maxNPets < 0 || maxNPets > 7) {
-        std::cerr << "ERROR: Incorrect number of pets specified" << std::endl;
+int main()
+{
+    int count_pet;
+    std::cin >> count_pet;
+    FamilyPets family;
+    if (count_pet > 7 || count_pet < 1)
+    {
+        std::cerr << "ERROR: Incorrect number of pets specified";
         return 1;
     }
-
-    FamilyPets pets;
-    int inner_counter = 0;
-    std::string input{"\0"};
-    std::string type, name, voice, character;
-    double weight;
-
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-    while (true) {
-        inner_counter++;
-        if (inner_counter > maxNPets) {
-            std::cerr << "ERROR: The number of pets exceeds the maximum" << std::endl;
+    int counter = 0;
+    while (true)
+    {
+        std::string type;
+        std::cin >> type;
+        if (type == "<END>")
+            break;
+        counter++;
+        if (counter > count_pet)
+        {
+            std::cerr << "ERROR: The number of pets exceeds the maximum";
             return 1;
         }
+        std::string name, voice;
+        std::size_t weight;
+        std::cin >> name >> voice >> weight; // Считываем данные для питомца
 
-        std::getline(std::cin, type);
-        if (type == "END") break;
-
-        std::getline(std::cin, name);
-        std::getline(std::cin, voice);
-        std::cin >> weight;
-        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-        if (type == "Cat" || type == "cat") {
-            pets += new Cat{name, voice, weight};
-        } else if (type == "Dog" || type == "dog") {
-            std::getline(std::cin, character);
-            pets += new Dog{name, voice, weight, character};
+        if (type == "Cat")
+        {
+            Cat *cat = new Cat(name, voice, weight);
+            family + cat;
+        }
+        else if (type == "Dog")
+        {
+            Dog *dog = new Dog(name, voice, weight);
+            family + dog;
         }
     }
-
-    std::cout << "Names of all pets:" << std::endl;
-    pets.names();
-
-    std::cout << "Voices of all pets:" << std::endl;
-    pets.voice();
-
-    std::cout << "Enter the name to search: ";
-    std::getline(std::cin, input);
-    std::cout << "Number of pets with name " << input << ": " << pets.getNumberRepetitionsName(input) << std::endl;
-
-    std::cout << "The easiest pet: " << pets.findTheEasiest() << std::endl;
-
-    std::cout << "Information about all pets:" << std::endl;
-    std::cout << pets;
-
-    return 0;
+    family.name();
+    family.voice();
+    std::string name;
+    std::cin >> name;
+    std::cout << name << ": " << family.getNumberRepetitionsName(name) << std::endl;
+    std::cout << family.findTheEasiest() << std::endl;
+    std::cout << family;
 }
